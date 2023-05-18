d4649dcc85b24ea592199bd9d66458fe2adfaff3
Anthony Lopez <Antl21292@gmail.com>
2023-05-17 23:22:19 -0400

    7 tests passing

--- caesar_cipher/cipher.py
+++ caesar_cipher/cipher.py
@@ -1,11 +1,34 @@
+import string
 
 
-def encrypt(text,shift):
-    pass
+def encrypt(text, shift):
+    """
+    Encrypts a message depending on the number of shifted values
+    :param text: A message
+    :param shift: An int
+    :return: An encrypted message
+    """
+    # creates a list of the alphabet
+    alphabet = list(string.ascii_lowercase)
+    # creates a list of the shifted alphabet
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    secret_message = ""
+
+    for char in text:
+        if char.lower() in alphabet:
+            if char.isupper():
+                index = alphabet.index(char.lower())
+                secret_message += shifted_alphabet[index].upper()
+            else:
+                index = alphabet.index(char)
+                secret_message += shifted_alphabet[index]
+        else:
+            secret_message += char
+    return secret_message
 
 
 def decrypt(text, shift):
-    pass
+    return encrypt(text, -shift)
 
 
 def crack(text):
